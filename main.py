472f7febe63ffc20f50a68b208ed62b0aa970413
Daniel Huang <daniel9huang@gmail.com>
2023-12-29 17:34:25 -0500

    add type validation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
 import string
 
+def validate_password_type(password_type: str) -> bool:
+    return False if password_type != 'random' or password_type != 'pin' else True
+
 def handle_random_password(password_length: int) -> str:
     characters: str = string.ascii_letters
     numbers: str = input('Numbers (y/n)? ')
@@ -19,15 +22,16 @@ def handle_pin_password(password_length: int) -> str:
 
 def main() -> None:
     PASSWORD_TYPE: str = input('Password type (random or pin): ')
+    if validate_password_type(PASSWORD_TYPE) == False:
+        raise Exception('Unsupported password type')
+    
     PASSWORD_LENGTH: int = int(input('Password length: '))
     password: str
 
     if PASSWORD_TYPE == 'random':
         password = handle_random_password(PASSWORD_LENGTH)
-    elif PASSWORD_TYPE == 'pin':
-        password = handle_pin_password(PASSWORD_LENGTH)
     else:
-        raise Exception('Unsupported password type')
+        password = handle_pin_password(PASSWORD_LENGTH)
 
     print(password)
 
